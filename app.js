bdae6bf4d6b8cb5184a4d286b4b74b4b61667e38
David Acevedo <davidacevedo@engineering.ucsb.edu>
2015-10-21 20:43:24 -0700

    Revert "Web rtc"

--- app.js
+++ app.js
@@ -10,33 +10,34 @@ var users = require('./routes/users');
 
 var app = express();
 
-/**
- * View engine setup
- */
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/**
- * Catch 404 response and send it to the error handler
- */
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-/**
- * Development error handler, it will print a stacktrace
- */
+// error handlers
+
+// development error handler
+// will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res) {
+    app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -45,16 +46,14 @@ if (app.get('env') === 'development') {
     });
 }
 
-/**
- * Production error handler, no stacktraces leaked to user
- */
-app.use(function(err, req, res) {
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/**
- * Gets the homepage from the jade files
- */
-router.get('/', function(req, res, next) {
+/* GET home page. */
+router.get('/', function(req, res, next){
     res.render('index', { title: 'Express' });
 });
 
