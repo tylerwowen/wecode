de8becb6389581f7463ceb116e526976af13d413
David <david@ResNet-22-2.resnet.ucsb.edu>
2015-10-21 18:17:52 -0700

    Added missing files from previous commit

--- app.js
+++ app.js
@@ -5,39 +5,35 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
+/**
+ * View engine setup
+ */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
+/**
+ * Catch 404 response and send it to the error handler
+ */
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
+/**
+ * Development error handler, it will print a stacktrace
+ */
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function(err, req, res) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -46,14 +42,16 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+/**
+ * Production error handler, no stacktraces leaked to user
+ */
+app.use(function(err, req, res) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
+
 
 module.exports = app;
--- public/javascripts/webRTC.js
+++ public/javascripts/webRTC.js
@@ -1,3 +1,14 @@
-/**
- * Created by david on 10/21/15.
- */
+var constraints = {video: true, audio: true};
+
+function successCallback(stream) {
+    window.stream = stream; // stream available to console
+    var video = document.querySelector("video");
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+}
+
+function errorCallback(error){
+    console.log("getUserMedia error: ", error);
+}
+
+getUserMedia(constraints, successCallback, errorCallback);
