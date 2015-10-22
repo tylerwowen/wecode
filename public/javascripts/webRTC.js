d5e475493d1304aceb84170ca8bbedae322be104
David Acevedo <davidacevedo@engineering.ucsb.edu>
2015-10-21 18:29:32 -0700

    Update webRTC.js with comments

--- public/javascripts/webRTC.js
+++ public/javascripts/webRTC.js
@@ -1,5 +1,11 @@
+/**
+ * Constraints define what user media the browser will use
+ */
 var constraints = {video: true, audio: true};
 
+/**
+ * Success callback for getUserMedia: If succesful, stream video on the browser
+ */ 
 function successCallback(stream) {
     window.stream = stream; // stream available to console
     var video = document.querySelector("video");
@@ -7,6 +13,9 @@ function successCallback(stream) {
     video.play();
 }
 
+/**
+ * Error handler for when things go wrong
+ */
 function errorCallback(error){
     console.log("getUserMedia error: ", error);
 }
