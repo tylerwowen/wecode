a4a6af18a6602da40ee1e6c822314368b07c235a
Jessica <jessicahuang@umail.ucsb.edu>
2015-11-29 23:45:02 -0800

    adding login box on main page (not done)

--- public/javascripts/app/main.js
+++ public/javascripts/app/main.js
@@ -2,7 +2,6 @@ define(function(require) {
     var $ = require('jquery'),
         EditorController = require('app/controller/editorcontroller');
     //var rtc = require('webrtc');
-    requirejs(['webrtc','draggableObjects', 'searchwindow', 'bootstrap']);
+    requirejs(['webrtc','draggableObjects', 'searchwindow', 'bootstrap', 'users']);
     var editorController = new EditorController();
-
 });
