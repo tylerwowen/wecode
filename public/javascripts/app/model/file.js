b814bdd0f86c637bad78a110cc0cb9b1592e432b
zhansaya <zhansaya@umail.ucsb.edu>
2016-01-12 21:34:19 -0800

    New files

---
+++ public/javascripts/app/model/file.js
@@ -0,0 +1,34 @@
+define(function(require) {
+    "use strict";
+
+    var RealTimeData = require('app/model/realtimedata');
+    var RealTimeUtils = require('lib/realtimeutils');
+    var GoogleFileAdapter = require('app/adapters/googlefileadapter');
+
+
+    function File(fileName) {
+        this.id = null;
+        this.name = fileName;
+        this.realTimeUtils = new RealTimeUtils();
+        this.realTimeData = new RealTimeData();
+        this.googleFileAdapter = new GoogleFileAdapter(this.realTimeData);
+    }
+
+    (function () {
+
+        this.load = function (id) {
+            this.googleFileAdapter.loadDriveFile(id);
+        };
+
+        this.deleteFile = function (id) {
+
+        };
+
+        this.createFile = function () {
+
+        };
+
+    }).call(File.prototype);
+
+    return File;
+});
---
+++ public/javascripts/app/model/folder.js
@@ -0,0 +1,24 @@
+define(function(require) {
+    "use strict";
+
+    function Folder() {
+        this.id = null;
+        this.name = null;
+    }
+
+    (function () {
+        this.load = function () {
+
+        };
+
+        this.deleteFolder = function (id) {
+
+        };
+
+        this.createFolder = function () {
+
+        };
+    }).call(Folder.prototype);
+
+    return Folder;
+});
