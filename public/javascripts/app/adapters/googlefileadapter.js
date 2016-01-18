dfc6ed0df53d5a6096386f0171651030f709f8dc
zhansaya <zhansaya@umail.ucsb.edu>
2016-01-17 23:01:14 -0800

    Changes

--- public/javascripts/app/adapters/googlefileadapter.js
+++ public/javascripts/app/adapters/googlefileadapter.js
@@ -1,12 +1,11 @@
 define(function(require) {
     "use strict";
 
-    var $ = require('jquery'),
+    var Q = require('q'),
         gapi = require('gapi');
 
-    function GoogleFileAdapter(file) {
-        this.file = file;
-    }
+    function GoogleFileAdapter() {}
+
 
     (function() {
 
@@ -18,28 +17,16 @@ define(function(require) {
          * @export
          */
 
-        // Function from RealtimeUtils:
-        // load: function(documentId, onFileLoaded, initializeModel) {
-        //    var that = this;
-        //    gapi.drive.realtime.load(documentId, function(doc) {
-        //        window.doc = doc;  // Debugging purposes
-        //        onFileLoaded(doc);
-        //    }, initializeModel, this.onError);
-        // },
-
         this.loadDriveFile = function(id) {
-            var that = this;
-            if (id) {
-                var isDataCached = this.file.connectWithEditor(id);
-                if(!isDataCached) {
-                    gapi.drive.realtime.load(id, function(doc) {
-                        that.file.onFileLoaded(doc, id);
-                    }, this.file.onFileInitialize. this.onError);
-                }
-                return true;
-            } else {
-                return false;
-            }
+            var deferred = Q.defer();
+            gapi.drive.realtime.load(id, function(doc) {
+                deferred.resolve(doc);
+            },function(){
+                deferred.resolve();
+            },function(error) {
+                deferred.reject(new Error(error));
+            });
+            return deferred.promise;
         };
 
     }).call(GoogleFileAdapter.prototype);
--- public/javascripts/app/model/realtimedata.js
+++ public/javascripts/app/model/realtimedata.js
@@ -1,54 +1,43 @@
 define(function(require) {
     "use strict";
-
-    var ACEAdapter = require('app/adapters/aceadapter'),
-        Cursor = require('app/model/cursor'),
-        gapi = require('gapi');
-
-    var bind = function (fn, me) {
-        return function () {
-            return fn.apply(me, arguments);
-        };
-    };
-
-    var data = {};
-
+    
     // Realtime data structure
     function RealTimeData() {
-        this.id = null;
         this.text = null;
         this.cursors = null;
         this.collaborators = null;
     }
-
-    function getCurrentUserId(collaborators) {
-        for (var i = 0; i < collaborators.length; i++) {
-            if (collaborators[i].isMe) {
-                return collaborators[i].userId;
+    (function () {
+        this.getCurrentUserId = function(collaborators) {
+            for (var i = 0; i < collaborators.length; i++) {
+                if (collaborators[i].isMe) {
+                    return collaborators[i].userId;
+                }
             }
-        }
-    }
+        };
 
-    function getColor(string) {
-        return '#' + intToRGB(hashCode(string));
-    }
+        this.getColor = function(string) {
+            return '#' + intToRGB(hashCode(string));
+        };
 
-    function hashCode(str) {
-        var hash = 0;
-        for (var i = 0; i < str.length; i++) {
-            hash = str.charCodeAt(i) + ((hash << 5) - hash);
-        }
-        return hash;
-    }
+        this.hashCode = function(str) {
+            var hash = 0;
+            for (var i = 0; i < str.length; i++) {
+                hash = str.charCodeAt(i) + ((hash << 5) - hash);
+            }
+            return hash;
+        };
 
-    function intToRGB(i) {
-        var c = (i & 0x00FFFFFF)
-            .toString(16)
-            .toUpperCase();
+        this.intToRGB = function(i) {
+            var c = (i & 0x00FFFFFF)
+                .toString(16)
+                .toUpperCase();
 
-        return "00000".substring(0, 6 - c.length) + c;
-    }
+            return "00000".substring(0, 6 - c.length) + c;
+        };
 
+    }).call(RealTimeData.prototype);
 
+    return RealTimeData;
 
 });
