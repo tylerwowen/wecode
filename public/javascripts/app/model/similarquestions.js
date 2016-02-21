4c7c0a8ecff93dbf71b82e8c906efaf8d14a170f
zhansaya <zhansaya@umail.ucsb.edu>
2016-02-20 16:43:23 -0800

    Added questions files for testing

--- public/javascripts/app/model/similarquestions.js
+++ public/javascripts/app/model/similarquestions.js
@@ -49,8 +49,44 @@ define(function (require) {
                 }
             }
 
-            console.log(this.queue[maxIndex].question);
             return this.queue[maxIndex].question;
+        };
+
+        this.getSimilarQuestionsWOTopics = function(currentquestion, queue) {
+            this.ques = currentquestion.toLowerCase();
+            this.queue = queue;
+
+            var array = this.ques.split(/\s+/);
+            var counter = 0;
+            var queueQues;
+            var countQuestArray = [];
+            for (var k = 0; k < this.queue.length; k++) {
+                queueQues = (this.queue[k].toLowerCase()).split(/\s+/);
+                counter = 0;
+                for (var i = 0; i < array.length; i++) {
+                    for (var j = 0; j < queueQues.length; j++) {
+                        var res = array[i].localeCompare(queueQues[j]);
+                        if(res == 0 ){
+                            counter++;
+                        }
+                    }
+                }
+                countQuestArray.push(counter);
+            }
+            var max = countQuestArray[0];
+            var maxIndex = 0;
+            var secondMaxIndex = 0;
+
+            for (var i = 1; i < countQuestArray.length; i++) {
+                if (countQuestArray[i] > max) {
+                    secondMaxIndex = maxIndex;
+                    maxIndex = i;
+                    max = countQuestArray[i];
+                }
+            }
+            var arr = [this.queue[maxIndex], this.queue[secondMaxIndex]];
+            //console.log(arr);
+            return arr;
         };
 
     }).call(SimilarQuestions.prototype);
